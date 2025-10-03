3af93f235397fce467100c7e4b5e9439ad5b948b
Flame Soulis <firecatdan@gmail.com>
2025-10-03 03:55:09 -0400

    Addressed pylint complaints and fixed line size comparisons

--- ascii_colorizer_lib.py
+++ ascii_colorizer_lib.py
@@ -1,64 +1,72 @@
 import os
 
 def check_file(file_name):
-	if not os.path.exists(file_name):
-		raise Exception("does not exist")
-	if not os.path.isfile(file_name):
-		raise Exception("is not a file")
+    if not os.path.exists(file_name):
+        raise Exception("does not exist")
+    if not os.path.isfile(file_name):
+        raise Exception("is not a file")
 
 def unique_symbols(ascii):
-	# Remove newlines and spaces
-	ascii = ascii.replace("\n", "")
-	ascii = ascii.replace(" ", "")
-	# Establish our list
-	return_list = []
-	# It's looping time!
-	for c in ascii:
-		# If the symbol isn't in the list, add it
-		if not c in return_list:
-			return_list.append(c)
-	# Return our list
-	return return_list
+    # Remove newlines and spaces
+    ascii = ascii.replace("\n", "")
+    ascii = ascii.replace(" ", "")
+    # Establish our list
+    return_list = []
+    # It's looping time!
+    for c in ascii:
+        # If the symbol isn't in the list, add it
+        if not c in return_list:
+            return_list.append(c)
+    # Return our list
+    return return_list
 
 def generate_mask(ascii, use_index = False):
-	# Grab the unique symbols
-	symbols = unique_symbols(ascii)
-	for i,c in enumerate(symbols):
-		ascii = ascii.replace(c, str((i + 1) * use_index))
-	return ascii
+    # Grab the unique symbols
+    symbols = unique_symbols(ascii)
+    for i,c in enumerate(symbols):
+        ascii = ascii.replace(c, str((i + 1) * use_index))
+    return ascii
 
 def generate_mask_file(source_file, use_index):
-	with open(source_file) as file_handle:
-		file_data = file_handle.read()
-		new_file_name = list(os.path.splitext(source_file))
-		new_file_name[0] += "_mask"
-		new_file_name = ''.join(new_file_name)
-		with open(new_file_name, 'w') as new_file_handle:
-			mask_data = generate_mask(file_data, use_index)
-			new_file_handle.write(mask_data)
+    with open(source_file, encoding="utf-8") as file_handle:
+        file_data = file_handle.read()
+        new_file_name = list(os.path.splitext(source_file))
+        new_file_name[0] += "_mask"
+        new_file_name = ''.join(new_file_name)
+        with open(new_file_name, 'w', encoding="utf-8") as new_file_handle:
+            mask_data = generate_mask(file_data, use_index)
+            new_file_handle.write(mask_data)
 
 
-def merge_mask(source_file, mask_file):
-	# make sure the files are the same size
-	if os.path.getsize(source_file) != os.path.getsize(mask_file):
-		raise Exception("Source file does not match size of mask file!")
-	with open(source_file) as source_handle:
-		with open(mask_file) as mask_handle:
-			new_ascii = ""
-			source_ascii = source_handle.read()
-			mask_data = mask_handle.read()
-			color_code = -1
-			# Break it down, line by line
-			source_lines = source_ascii.split("\n")
-			mask_lines = mask_data.split("\n")
-			for index, current_line in enumerate(source_lines):
-				for letter_index in range(len(current_line)):
-					#Check if our color code has changed and isn't space
-					if mask_lines[index][letter_index] != color_code:
-						if mask_lines[index][letter_index] != ' ':
-							color_code = mask_lines[index][letter_index]
-							new_ascii += f"${color_code}"
-					new_ascii += current_line[letter_index]
-				new_ascii += "\n"
-	with open(source_file+"_output", 'w') as new_ascii_handle:
-		new_ascii_handle.write(new_ascii)
+def merge_mask(source_file):
+    # get the mask's name
+    mask_file = list(os.path.splitext(source_file))
+    mask_file[0] += "_mask"
+    mask_file = ''.join(mask_file)
+    # generate the new filename for later
+    new_file_name = list(os.path.splitext(source_file))
+    new_file_name[0] += "_output"
+    new_file_name = ''.join(new_file_name)
+    # begin the loop
+    with open(source_file, encoding="utf-8") as source_handle:
+        with open(mask_file, encoding="utf-8") as mask_handle:
+            new_ascii = ""
+            source_ascii = source_handle.read()
+            mask_data = mask_handle.read()
+            color_code = -1
+            # Break it down, line by line
+            source_lines = source_ascii.split("\n")
+            mask_lines = mask_data.split("\n")
+            for index, current_line in enumerate(source_lines):
+                if len(current_line) is not len(mask_lines[index]):
+                    raise ValueError(f"Line {index} does not match in length!")
+                for letter_index, current_letter in enumerate(current_line):
+                    #Check if our color code has changed and isn't space
+                    if mask_lines[index][letter_index] != color_code:
+                        if mask_lines[index][letter_index] != ' ':
+                            color_code = mask_lines[index][letter_index]
+                            new_ascii += f"${color_code}"
+                    new_ascii += current_letter
+                new_ascii += "\n"
+    with open(new_file_name, 'w', encoding="utf-8") as new_ascii_handle:
+        new_ascii_handle.write(new_ascii)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 from ascii_colorizer_lib import *
 
 USE_INDEX_NUMBERS 	= 0b0001
@@ -6,81 +7,81 @@ ALLOW_OVERRIDE 		= 0b0010
 MERGE_MASK			= 0b0100
 
 def print_usage():
-	print("ASCII Colorizer Assist Tool")
-	print(f"Usage: {os.path.basename(__file__)} [options] file")
-	print()
-	print("Options:")
-	print("-n, --number\tGenerates mask using indexes (non-zero)")
-	print("-o, --override\tForces mask generation (overrides existing)")
-	print("-m, --merge\tMerges an existing mask with file")
+    print("ASCII Colorizer Assist Tool")
+    print(f"Usage: {os.path.basename(__file__)} [options] file")
+    print()
+    print("Options:")
+    print("-n, --number\tGenerates mask using indexes (non-zero)")
+    print("-o, --override\tForces mask generation (overrides existing)")
+    print("-m, --merge\tMerges an existing mask with file")
 
 def gen_line(length):
-	string = ""
-	while len(string) < length:
-		string += "-"
-	return string
+    string = ""
+    while len(string) < length:
+        string += "-"
+    return string
 
 def error_msg(err_msg):
-	print(err_msg)
-	print(gen_line(len(err_msg)))
-	print_usage()
+    print(err_msg)
+    print(gen_line(len(err_msg)))
+    print_usage()
 
 def validate_param(param):
-	# Lower everything
-	param = param.lower()
-	# Are we dealing with a - or --?
-	if param[0:1] == "--":
-		# Here comes the fun!
-		match param[2:]:
-			case "number":
-				return USE_INDEX_NUMBERS
-			case "override":
-				return ALLOW_OVERRIDE
-			case "merge":
-				return MERGE_MASK
-	elif param[0] == "-":
-		# Oh boy!
-		flags = 0
-		for c in param[1:]:
-			if c == "n":
-				flags |= USE_INDEX_NUMBERS
-			elif c == "o":
-				flags |= ALLOW_OVERRIDE
-			elif c == "m":
-				flags |= MERGE_MASK
-			else:
-				return False
-		return flags
-	else:
-		# Who are you?!
-		return False
+    # Lower everything
+    param = param.lower()
+    # Are we dealing with a - or --?
+    if param[0:1] == "--":
+        # Here comes the fun!
+        match param[2:]:
+            case "number":
+                return USE_INDEX_NUMBERS
+            case "override":
+                return ALLOW_OVERRIDE
+            case "merge":
+                return MERGE_MASK
+    elif param[0] == "-":
+        # Oh boy!
+        flags = 0
+        for c in param[1:]:
+            if c == "n":
+                flags |= USE_INDEX_NUMBERS
+            elif c == "o":
+                flags |= ALLOW_OVERRIDE
+            elif c == "m":
+                flags |= MERGE_MASK
+            else:
+                return False
+        return flags
+    else:
+        # Who are you?!
+        return False
 
 
 if __name__ == "__main__":
-	# If we don't have enough arguments, explain how to use
-	if len(sys.argv) < 2:
-		print_usage()
-		sys.exit(2)
+    # If we don't have enough arguments, explain how to use
+    if len(sys.argv) < 2:
+        print_usage()
+        sys.exit(2)
 
-	# Does the file exist?
-	ascii_file = sys.argv[-1]
-	try:
-		check_file(ascii_file)
-	except Exception as e:
-		error_msg(f"{ascii_file} {e}")
-		sys.exit(2)
+    # Does the file exist?
+    ascii_file = sys.argv[-1]
+    try:
+        check_file(ascii_file)
+    except Exception as e:
+        error_msg(f"{ascii_file} {e}")
+        sys.exit(2)
 
-	# Check the params
-	FLAGS = 0
-	for i in range(1,len(sys.argv) - 1):
-		valid_param = validate_param(sys.argv[i])
-		if not valid_param:
-			error_msg(f"{sys.argv[i]} is not a valid parameter!")
-			sys.exit(3)
-		FLAGS |= valid_param
+    # Check the params
+    FLAGS = 0
+    for i in range(1,len(sys.argv) - 1):
+        valid_param = validate_param(sys.argv[i])
+        if not valid_param:
+            error_msg(f"{sys.argv[i]} is not a valid parameter!")
+            sys.exit(3)
+        FLAGS |= valid_param
 
-	# Let's do this!
-	if not (FLAGS & MERGE_MASK):
-		generate_mask_file(ascii_file, FLAGS & USE_INDEX_NUMBERS)
-	else:
-		merge_mask(ascii_file, ascii_file+"_mask")
+    # Let's do this!
+    if not (FLAGS & MERGE_MASK):
+        generate_mask_file(ascii_file, FLAGS & USE_INDEX_NUMBERS)
+    else:
+        merge_mask(ascii_file)
